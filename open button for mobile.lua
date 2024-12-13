c3e9049757b637dae8148e2d831fcd1fda8c87db
screengui <134831841+screengui@users.noreply.github.com>
2024-12-13 17:40:48 +0800

    Update open button for mobile.lua

--- open button for mobile.lua
+++ open button for mobile.lua
@@ -3,44 +3,35 @@ local VirtualInputManager = game:GetService("VirtualInputManager")
 
 local frameSize = UDim2.new(0, 64, 0, 64)
 local framePosition = UDim2.new(0.5, 0, 0.5, 0) -- Centered
-local frameColor = Color3.fromRGB(64,64,64) -- Gray
+local frameColor = Color3.fromRGB(24,24,24) -- Gray
 
 local screenGui = Instance.new("ScreenGui")
 screenGui.Name = "DraggableFrameGui"
 screenGui.ResetOnSpawn = false
 screenGui.DisplayOrder = 10
 
-local frame = Instance.new("Frame")
-frame.Name = "DraggableFrame"
-frame.Size = frameSize
-frame.Position = framePosition
-frame.BackgroundColor3 = frameColor
-frame.BorderSizePixel = 0
-frame.ZIndex = 0 
-frame.Selectable = true 
-frame.Active = true 
-frame.Draggable = true
-
-local frameCorner = Instance.new("UICorner")
-frameCorner.CornerRadius = UDim.new(0.2, 0) 
-frameCorner.Parent = frame
-
 local buttonSize = UDim2.new(0, 45, 0, 45)
 local button = Instance.new("TextButton")
 button.Name = "Button"
-button.Size = buttonSize
+button.Size = frameSize
 button.AnchorPoint = Vector2.new(0.5, 0.5)
-button.Position = UDim2.new(0.5, 0, 0.5, 0)
-button.BackgroundTransparency = 1
-button.Parent = frame
+button.Position = framePosition
+button.BackgroundColor3 = frameColor
+button.Parent = screenGui
 button.Text = "Open"
 button.TextColor3 = Color3.fromRGB(255, 255, 255)
+button.Font = Enum.Font.FredokaOne
+button.TextScaled = true
 button.ZIndex = 0
 button.Active = true
+button.Draggable = true
+
+local frameCorner = Instance.new("UICorner")
+frameCorner.CornerRadius = UDim.new(0.2, 0) 
+frameCorner.Parent = button
 
 button.MouseButton1Click:Connect(function()
-  VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.RightShift, false, game)
+  VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.LeftControl, false, game)
 end)
 
-frame.Parent = screenGui
 screenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")

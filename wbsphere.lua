ec7d0143e1558e74e233fc78682c0f07517f963f
screengui <134831841+screengui@users.noreply.github.com>
2025-02-08 20:34:48 +0800

    Update wbsphere.lua

--- wbsphere.lua
+++ wbsphere.lua
@@ -14,13 +14,13 @@ end
 local Base = findPlot(game.Players.LocalPlayer.Name).BasePlate
 local basePosition = Base.Position -- Center of the plot
 
--- Function to place a block in a **spherical pattern**
+-- Function to place a block in a **spherical pattern** from bottom to top
 function placeBlockInSphere(id, radius, numBlocks, yOffset)
     local blocksPlaced = 0
     local phiIncrement = math.pi * (3 - math.sqrt(5)) -- Golden angle approximation for even distribution
     for i = 0, numBlocks - 1 do
         -- Calculate spherical coordinates
-        local y = 1 - (i / (numBlocks - 1)) * 2 -- Y ranges from 1 to -1
+        local y = -1 + (i / (numBlocks - 1)) * 2 -- Y ranges from -1 to 1 (bottom to top)
         local radiusAtY = math.sqrt(1 - y * y) * radius -- Radius at given Y height
         local theta = phiIncrement * i -- Angle around the Z-axis
 

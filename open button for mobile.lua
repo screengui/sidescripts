496cb380e72fa22a19bc59c20534632830b5ae7d
screengui <134831841+screengui@users.noreply.github.com>
2026-04-07 22:22:13 +0800

    Update open button for mobile.lua

--- open button for mobile.lua
+++ open button for mobile.lua
@@ -27,7 +27,7 @@ Image.Name = "Icon"
 Image.Parent = Toggle
 Image.Size = UDim2.new(1, 0, 1, 0)
 Image.BackgroundTransparency = 1
-Image.Image = "rbxassetid://113009383175910" 
+Image.Image = "rbxassetid://72731518878780" 
 
 local Corner2 = Instance.new("UICorner")
 Corner2.CornerRadius = UDim.new(0.2, 0)
